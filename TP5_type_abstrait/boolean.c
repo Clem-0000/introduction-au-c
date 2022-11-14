edb919658523c98b0ee6e58f14b3b796edeb407c
Clem-0000 <clemtoon78700.bri@gmail.com>
2022-11-14 18:17:26 +0100

    Changes by me
    Co-authored-by: Arthur PELLEGRINI <arthur.pellegrini@edu.esiee-it.fr>

---
+++ TP5_type_abstrait/boolean.c
@@ -0,0 +1,21 @@
+// Exercice 1
+/* ******************************
+ * Function TRUE
+ * ******************************/
+int true() { return 1; }
+
+int false() {
+    return 0;
+}
+
+int not(int bool) {
+    return !bool;
+}
+
+int and(int bool1, int bool2) {
+    return bool1 && bool2;
+}
+
+int or(int bool1, int bool2) {
+    return bool1 || bool2;
+}
